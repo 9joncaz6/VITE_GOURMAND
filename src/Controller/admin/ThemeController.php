43daf845dce682b25f9a0fb9af052f86ca6bcb24
9joncaz6 <mayjoca789@gmail.com>
2026-05-14 16:01:31 +0200

    Corrections themes

--- src/Controller/admin/ThemeController.php
+++ src/Controller/admin/ThemeController.php
@@ -12,20 +12,19 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-
 #[IsGranted('ROLE_EMPLOYE')]
 #[Route('/admin/theme')]
 final class ThemeController extends AbstractController
 {
-    #[Route(name: 'app_theme_index', methods: ['GET'])]
+    #[Route('/', name: 'admin_theme_index', methods: ['GET'])]
     public function index(ThemeRepository $themeRepository): Response
     {
-        return $this->render('theme/index.html.twig', [
+        return $this->render('admin/theme/index.html.twig', [
             'themes' => $themeRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_theme_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'admin_theme_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $theme = new Theme();
@@ -37,24 +36,24 @@ final class ThemeController extends AbstractController
             $entityManager->flush();
 
             $this->addFlash('success', 'Thème créé avec succès.');
-            return $this->redirectToRoute('app_theme_index');
+            return $this->redirectToRoute('admin_theme_index');
         }
 
-        return $this->render('theme/new.html.twig', [
+        return $this->render('admin/theme/new.html.twig', [
             'theme' => $theme,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_theme_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'admin_theme_show', methods: ['GET'])]
     public function show(Theme $theme): Response
     {
-        return $this->render('theme/show.html.twig', [
+        return $this->render('admin/theme/show.html.twig', [
             'theme' => $theme,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_theme_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'admin_theme_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Theme $theme, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ThemeType::class, $theme);
@@ -64,24 +63,25 @@ final class ThemeController extends AbstractController
             $entityManager->flush();
 
             $this->addFlash('success', 'Thème modifié avec succès.');
-            return $this->redirectToRoute('app_theme_index');
+            return $this->redirectToRoute('admin_theme_index');
         }
 
-        return $this->render('theme/edit.html.twig', [
+        return $this->render('admin/theme/edit.html.twig', [
             'theme' => $theme,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_theme_delete', methods: ['POST'])]
+    #[Route('/{id}/delete', name: 'admin_theme_delete', methods: ['POST'])]
     public function delete(Request $request, Theme $theme, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$theme->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($theme);
             $entityManager->flush();
+
             $this->addFlash('success', 'Thème supprimé avec succès.');
         }
 
-        return $this->redirectToRoute('app_theme_index');
+        return $this->redirectToRoute('admin_theme_index');
     }
 }
