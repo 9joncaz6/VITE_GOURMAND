10175261787f29d51c3a2882e57a902d9a4258ca
9joncaz6 <mayjoca789@gmail.com>
2026-05-19 17:57:58 +0200

    Public index

--- public/index.php
+++ public/index.php
@@ -5,8 +5,9 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-// Charge les variables d'environnement (.env, .env.local, etc.)
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+if ($_SERVER['APP_ENV'] !== 'prod') {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
