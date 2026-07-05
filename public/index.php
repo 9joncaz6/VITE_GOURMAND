c03c1eda9bca19be8e72cef819cd6cecd66a379e
9joncaz6 <mayjoca789@gmail.com>
2026-07-05 21:05:38 +0200

    Nettoyage des données privées

--- public/index.php
+++ public/index.php
@@ -1,34 +1,4 @@
 <?php
 
-use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload.php';
-
-/**
- * Render Free Hack :
- * Les variables d'environnement ne sont pas injectées correctement.
- * On les force ici pour garantir que Symfony les voit.
- */
-putenv('MONGODB_URL=mongodb+srv://mayjoca789_db_user:n86DcKTrXa8QWAA1@cluster0.f9crcxj.mongodb.net/vitegourmand?retryWrites=true&w=majority&appName=Cluster0');
-putenv('MONGODB_DB=vitegourmand');
-putenv('DEFAULT_URI=https://vitegourmand.onrender.com');
-putenv('DATABASE_URL=mysql://jonca:1PaxAugusta5@mysql-jonca.alwaysdata.net:3306/jonca_vitegourmand?serverVersion=mariadb-11.4.0&charset=utf8mb4');
-
-// IMPORTANT : on NE met PAS le MAILER_DSN ici
-// Render ne peut pas contacter AlwaysData → timeout → blocage de la commande
-// Le mailer sera désactivé via MAILER_DSN=null://null dans Render
-
-$env = getenv('APP_ENV') ?: 'prod';
-$debug = getenv('APP_DEBUG') === '1';
-
-if ($env !== 'prod') {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
-
-$kernel = new Kernel($env, $debug);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+echo "Vite Gourmand - Dépôt public (backend retiré)";
